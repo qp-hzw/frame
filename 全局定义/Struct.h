6df2b51774ca7832d9bafea5c13be75e1ab84fcd
wcq <377133665@qq.com>
2020-02-24 20:19:57 +0800

    结构体 定义位置修改

--- 全局定义/Struct.h
+++ 全局定义/Struct.h
@@ -3,6 +3,40 @@
 #pragma pack(1)
 #include "Define.h"
 
+//单个比赛场配置
+struct MATCH_CONFIG
+{
+	//阶段信息
+	struct STAGE_INFO
+	{
+		TCHAR	szName[16];	//初赛、半决赛、决赛..
+		DWORD	wXJCount;					//小局数
+		DWORD	wPlayerCount;				//人数
+		DWORD	wJinJi_Rule;				//晋级规则   同桌比较/全桌比较 1,2,
+		DWORD	wJinJi_Count;				//晋级名额
+	};
+
+	//比赛场信息
+	DWORD		wMatchID;							//比赛场ID标识
+	TCHAR		szTitle[32];			//标题
+	TCHAR		szDescribe[128];		//比赛描述
+	STAGE_INFO	stage[5];				//阶段信息
+	DWORD		wStageSize;							//阶段个数
+	DWORD		llBaseScore;						//底分
+
+	//比赛场属性
+	DWORD		dwStartTime;						//开始时间 （-1表示人满即开）
+	DWORD		dwTimePeriod;						//创建周期
+	DWORD		wStartType;							//开始类型  (0-人满开始  1-定时开始)
+	DWORD		dwLowestPlayer;						//最少参赛人数
+	DWORD		FeeType;							//报名费类型 0-金币 1-砖石 2-门票
+	DWORD		llFee;								//报名费
+	DWORD		llReword;							//奖励
+
+	//机器人信息
+	DWORD		dwRobotCount;						//机器人数量
+};
+
 //用户道具 -- 与client保持一致
 struct tagUserProperty
 {
